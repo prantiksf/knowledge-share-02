465b27ffc9cc457cf23c8efdc2c9f01f4d4fb1a5
Prantik Banerjee <prantik.banerjee@prantik-ltm2chv.internal.salesforce.com>
2025-09-08 00:40:19 +0530

    Add Conv AI Nav + Combined view

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'combined.html'));
+});
+
+app.get('/topbar', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/nav', (req, res) => {
+    res.sendFile(path.join(__dirname, 'nav.html'));
 });
 
 app.listen(PORT, () => {
